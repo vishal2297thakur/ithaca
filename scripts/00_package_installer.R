b2217e789bccfe59e7cbc8ec0dbd7b21c8223a91
yannis <imarkonis@gmail.com>
2022-08-20 19:03:34 +0000

    Added script for package installation and improved first_check

---
+++ scripts/00_package_installer.R
@@ -0,0 +1,10 @@
+#Check and installation of the necessary packages for the project
+
+options(repos = 'http://cran.rstudio.org')
+have_packages <- installed.packages()
+cran_packages <- c('data.table', 'tidyverse', 'dbplyr', 
+                   'foreach', 'parallel', 'doParallel',
+                   'raster', 'sf', 'sfc', 'rgdal', 'RNetCDF',
+                   'RPostgres', 'rpostgis')
+to_install <- setdiff(cran_packages, have_packages[, 1])
+if(length(to_install)>0) install.packages(to_install)
--- scripts/01_first_check.R
+++ scripts/01_first_check.R
@@ -1,15 +1,19 @@
+#Quick check of data directory consistency and downloaded data content
+
 library(RNetCDF)
 library(raster)
 
 em_earth_prcp <- stack('../../shared/data/obs/precip/em_earth.tp.mm.global.195001.201912.025.monthly.nc')
-for(i in 1:12){
-  image(em_earth_prcp[[i]],
+
+random_months <- sample(1:100, 12)
+for(count_month in random_months){
+  image(em_earth_prcp[[count_month]],
         col = hcl.colors(12, "Blues3", rev = TRUE))
 }
 
 era5_cloud <- stack('../../shared/data/sim/cloudcover/global/raw/era5.tcc.frac.global.195901.202112.025.monthly.nc.nc')
-for(i in 1:12){
-  image(era5_cloud[[i]], 
+for(count_month in random_months){
+  image(era5_cloud[[count_month]], 
         col = hcl.colors(12, "Blues3"))
 }
 
