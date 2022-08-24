2c2c742c26c9b25209da1233930196587675bf3a
Yannis Markonis <imarkonis@gmail.com>
2022-08-24 11:22:43 +0200

    Update 01_first_check.R

--- scripts/01_first_check.R
+++ scripts/01_first_check.R
@@ -6,14 +6,14 @@ library(raster)
 em_earth_prcp <- stack('../../shared/data/obs/precip/em_earth.tp.mm.global.195001.201912.025.monthly.nc')
 
 random_months <- sample(1:100, 12)
-for(count_month in random_months){
-  image(em_earth_prcp[[count_month]],
+for(a_month in random_months){
+  image(em_earth_prcp[[a_month]],
         col = hcl.colors(12, "Blues3", rev = TRUE))
 }
 
 era5_cloud <- stack('../../shared/data/sim/cloudcover/global/raw/era5.tcc.frac.global.195901.202112.025.monthly.nc.nc')
-for(count_month in random_months){
-  image(era5_cloud[[count_month]], 
+for(a_month in random_months){
+  image(era5_cloud[[a_month]], 
         col = hcl.colors(12, "Blues3"))
 }
 
