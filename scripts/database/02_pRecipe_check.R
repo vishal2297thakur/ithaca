88b04645310906773b17ef3f89a8586f9101a6ad
yannis <imarkonis@gmail.com>
2022-09-26 15:35:58 +0000

    pRecipe testing

--- scripts/database/02_pRecipe_check.R
+++ scripts/database/02_pRecipe_check.R
@@ -20,9 +20,29 @@ subset_spacetime("era5", start_year = 1981, end_year = 2020,
                  database_path = paste0(path_data, 'sim/precip/raw/'))
 
 make_ts("era5", database_path = "./data/database/")
-tp_ts <- readr::read_csv("./data/processed/era5_ts.csv")
+tp_ts <- readr::read_csv("./data/processed/era5_tp_mm_global_195901_202112_025_monthly_ts.csv")
 head(tp_ts, 12)
 
+era5_subset <- raster::brick("./data/processed/era5_tp_mm_subset_1981_2020_025_monthly.nc")
+make_ts(era5_subset)
+subset_ts <- readr::read_csv("./data/processed/era5_tp_mm_subset_1981_2020_025_monthly_ts.csv")
+head(subset_ts, 12)
+
+
+global <- raster::raster("./data/database/era5_tp_mm_global_195901_202112_025_monthly.nc")
+plot_map(global[[1]])
+
+central_europe <- raster::brick("./data/processed/era5_tp_mm_subset_1981_2020_025_monthly.nc")
+plot_map(central_europe[[1]])
+
+plot_line(tp_ts)
+plot_heatmap(subset_ts)
+plot_box(subset_ts)
+
+
+
+
+#
 show_info(path_era5)
 make_ts("era5", database_path = "./data/database/")
 tp_ts <- readr::read_csv("./data/processed/era5_ts.csv")
