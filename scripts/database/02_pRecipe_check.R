ae776239edfa0ff723e851f8f55e543a4bb9acbc
yannis <imarkonis@gmail.com>
2022-09-26 15:54:17 +0000

    avoid future downloads

--- scripts/database/02_pRecipe_check.R
+++ scripts/database/02_pRecipe_check.R
@@ -6,18 +6,18 @@ library(pRecipe)
 
 path_era5 <- paste0(path_data, 'sim/precip/raw/era5_tp_mm_global_195901_202112_025_monthly.nc')
 
-download_data(name = "era5", project_folder = ".")
+#download_data(name = "era5", project_folder = ".")
 show_info("./data/database/era5_tp_mm_global_195901_202112_025_monthly.nc")
 
-subset_spacetime("era5", start_year = 1981, end_year = 2020, 
-                 bbox = c(2,28,42,58), 
-                 database_path = "./data/database/")
+#subset_spacetime("era5", start_year = 1981, end_year = 2020, 
+#                 bbox = c(2,28,42,58), 
+#                 database_path = "./data/database/")
 show_info("./data/processed/era5_tp_mm_subset_1981_2020_025_monthly.nc")
 
 
-subset_spacetime("era5", start_year = 1981, end_year = 2020, 
-                 bbox = c(2,28,42,58), 
-                 database_path = paste0(path_data, 'sim/precip/raw/'))
+#subset_spacetime("era5", start_year = 1981, end_year = 2020, 
+#                 bbox = c(2,28,42,58), 
+#                 database_path = paste0(path_data, 'sim/precip/raw/'))
 
 make_ts("era5", database_path = "./data/database/")
 tp_ts <- readr::read_csv("./data/processed/era5_tp_mm_global_195901_202112_025_monthly_ts.csv")
