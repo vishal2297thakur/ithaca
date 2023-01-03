1a78671937cc23d117a676616dc80967a2aeee4c
Yannis Markonis <imarkonis@gmail.com>
2023-01-03 12:28:41 +0100

    Update 05_assess_uncertainty.R

--- example_kenya/05_assess_uncertainty.R
+++ example_kenya/05_assess_uncertainty.R
@@ -1,7 +1,2 @@
-
 source('source/example_kenya.R')
 source('source/geo_functions.R')
-
-prec_stats <- readRDS(paste0(path_save_kenya, "prec_stats.rds"))
-evap_stats <- readRDS(paste0(path_save_kenya, "evap_stats.rds"))
-
