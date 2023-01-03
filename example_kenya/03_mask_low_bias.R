6f7cae58492e9d1448f145e4c5b3b69d1cc16398
imarkonis <you@example.comimarkonis@gmail.com>
2023-01-03 17:43:04 +0000

    fixed expand bug working on graphics

--- example_kenya/03_mask_low_bias.R
+++ example_kenya/03_mask_low_bias.R
@@ -1,6 +1,6 @@
-
 source('source/example_kenya.R')
 source('source/geo_functions.R')
+source('source/graphics.R')
 
 # Read data 
 prec_stats <- readRDS(paste0(path_save_kenya, "prec_stats.rds"))
@@ -20,21 +20,23 @@ saveRDS(all_stats, paste0(path_save_kenya, "all_stats.rds"))
 saveRDS(all_stats_mean, paste0(path_save_kenya, "all_stats_mean.rds"))
 saveRDS(all_stats_low_bias, paste0(path_save_kenya, "all_stats_low_bias.rds"))
 
+all_stats_low_bias <- readRDS(paste0(path_save_kenya, "all_stats_low_bias.rds"))
+
 # Plot data
 to_plot <- all_stats_low_bias[variable == 'prec']
-p00 <- ggplot(to_plot) +
+p00 <- ggplot(to_plot, aes(fill = 'red')) +
   geom_raster(aes(x = lon, y = lat, fill = "")) +
   borders(colour = "black") +
   coord_cartesian(xlim = c(min(to_plot$lon), max(to_plot$lon)), 
                   ylim = c(min(to_plot$lat), max(to_plot$lat))) +  
   labs(x = "", y = "", fill = 'Low bias') +
-  scale_x_continuous(expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0.015, 0.015)) +
   theme_bw() +
   theme(panel.background = element_rect(fill = NA), panel.ontop = TRUE,
         panel.grid = element_line(color = "grey"))
 y_labs <- ggplot_build(p00)$layout$panel_params[[1]]$y$get_labels()
 x_labs <- ggplot_build(p00)$layout$panel_params[[1]]$x$get_labels()
-p01 <- p00 + scale_x_continuous(expand = c(0, 0), labels = paste0(x_labs, "\u00b0")) +
+p01 <- p00 + scale_x_continuous(expand = c(0.015, 0.015), labels = paste0(x_labs, "\u00b0")) +
   scale_y_continuous(expand = c(0.0125, 0.0125),  labels = paste0(y_labs, "\u00b0"))
 p01
 
