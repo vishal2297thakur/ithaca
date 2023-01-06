a68e5355920d1cafa576bc19736769aa25ba7d5a
imarkonis <you@example.comimarkonis@gmail.com>
2023-01-06 10:10:35 +0000

    Correction in the estimation of joint bias

--- blueprint/03_mask_low_bias.R
+++ blueprint/03_mask_low_bias.R
@@ -8,9 +8,18 @@ source('source/graphics.R')
 ## Read data 
 prec_stats <- readRDS(paste0(path_save_blueprint, "ensemble_prec_stats.rds"))
 
+## Set variables
+quantiles <- seq(0.1, 1, 0.1)
+bias_thres_cv <- 0.8
+
 ## Main estimations
 prec_stats_mean <- prec_stats[, lapply(.SD, mean), .SDcols = c('mean', 'sd', 'cv'), by = c('lon', 'lat')]
-prec_stats_low_bias <- prec_stats_mean[cv < 0.5,  .(lon, lat)]
+prec_stats_low_bias <- prec_stats_mean[cv <= bias_thres_cv,  .(lon, lat)]
+
+cv_quantiles <- prec_stats_mean[, quantile(cv, quantiles)]
+cv_values_n <- data.table(quantile = quantiles, 
+                          cv = cv_quantiles, 
+                          size = floor(quantile(1:nrow(prec_stats_mean), quantiles)))
 
 ## Save data for further use
 saveRDS(prec_stats_mean, paste0(path_save_blueprint, "prec_stats_mean.rds"))
@@ -34,4 +43,14 @@ x_labs <- ggplot_build(p00)$layout$panel_params[[1]]$x$get_labels()
 p01 <- p00 + scale_x_continuous(expand = c(0.015, 0.015), labels = paste0(x_labs, "\u00b0")) +
   scale_y_continuous(expand = c(0.0125, 0.0125),  labels = paste0(y_labs, "\u00b0"))
 p01
+
+to_plot <- cv_values_n
+ggplot(to_plot, aes(y = size, x = cv)) +
+  geom_point() +
+  geom_line() +
+  labs(y = "Size", x = "CV") +
+  theme_light() +
+  theme(panel.background = element_rect(fill = NA), panel.ontop = TRUE,
+        panel.grid = element_line(color = "grey")) 
+
 
--- example_kenya/03_mask_low_bias.R
+++ example_kenya/03_mask_low_bias.R
@@ -9,6 +9,10 @@ source('source/graphics.R')
 prec_stats <- readRDS(paste0(path_save_kenya, "ensemble_prec_stats.rds"))
 evap_stats <- readRDS(paste0(path_save_kenya, "ensemble_evap_stats.rds"))
 
+## Set variables
+quantiles <- seq(0.1, 1, 0.1)
+bias_thres_cv <- 0.5
+
 ## Merge data 
 prec_stats[, variable := 'prec']
 evap_stats[, variable := 'evap']
@@ -16,7 +20,17 @@ all_stats <- rbind(prec_stats, evap_stats)
 
 ## Main estimations
 all_stats_mean <- all_stats[, lapply(.SD, mean), .SDcols = c('mean', 'sd', 'cv'), by = c('lon', 'lat', 'variable')]
-all_stats_low_bias <- all_stats_mean[cv < 0.5,  .(lon, lat, variable)]
+all_stats_low_bias <- all_stats_mean[cv < bias_thres_cv,  .(lon, lat, variable)]
+all_stats_low_bias[, all_low_bias := .N == n_variables, by =  c('lon', 'lat')]
+all_stats_mean <- merge(all_stats_mean, all_stats_low_bias, by = c('lon', 'lat', 'variable'))
+
+grid_cell_n <- nrow(all_stats_mean[variable == 'prec'])
+     
+cv_quantiles <- all_stats_mean[, .(cv = quantile(cv, quantiles)), variable]
+cv_values_n <- data.table(quantile = quantiles, 
+                          variable = cv_quantiles$variable,
+                          cv = cv_quantiles$cv, 
+                          size = floor(quantile(1:grid_cell_n, quantiles)))
 
 ## Save data for further use
 saveRDS(all_stats, paste0(path_save_kenya, "ensemble_stats.rds"))
@@ -24,6 +38,16 @@ saveRDS(all_stats_mean, paste0(path_save_kenya, "ensemble_stats_mean.rds"))
 saveRDS(all_stats_low_bias, paste0(path_save_kenya, "ensemble_stats_low_bias.rds"))
 
 ## Plot results
+to_plot <- cv_values_n
+ggplot(to_plot, aes(x = size, y = cv, col = variable)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "Size", y = "CV") +
+  scale_color_manual(values = c(main_cols[3], main_cols[1])) +
+  theme_light() +
+  theme(panel.background = element_rect(fill = NA), panel.ontop = TRUE,
+        panel.grid = element_line(color = "grey")) 
+
 to_plot <- all_stats_low_bias[variable == 'prec']
 p00 <- ggplot(to_plot) +
   geom_raster(aes(x = lon, y = lat, fill = "")) +
@@ -59,7 +83,7 @@ p01 <- p00 + scale_x_continuous(expand = c(0, 0), labels = paste0(x_labs, "\u00b
   scale_y_continuous(expand = c(0.0125, 0.0125),  labels = paste0(y_labs, "\u00b0"))
 p01
 
-to_plot <- unique(all_stats_low_bias[, .(lon, lat)])
+to_plot <- all_stats_low_bias[all_low_bias == TRUE]
 p00 <- ggplot(to_plot) +
   geom_raster(aes(x = lon, y = lat, fill = "")) +
   borders(colour = "black") +
@@ -75,4 +99,7 @@ x_labs <- ggplot_build(p00)$layout$panel_params[[1]]$x$get_labels()
 p01 <- p00 + scale_x_continuous(expand = c(0, 0), labels = paste0(x_labs, "\u00b0")) +
   scale_y_continuous(expand = c(0.0125, 0.0125),  labels = paste0(y_labs, "\u00b0"))
 p01
+
+
+
 
