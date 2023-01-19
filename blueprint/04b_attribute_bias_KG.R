2f0f3bee89063cb15aa079f2c7d5f60d2516853d
imarkonis <you@example.comimarkonis@gmail.com>
2023-01-19 13:42:02 +0000

    playing around

--- blueprint/04b_attribute_bias_KG.R
+++ blueprint/04b_attribute_bias_KG.R
@@ -23,14 +23,9 @@ ggplot() +
   geom_raster(data = prec_mask_mean, aes(lon, lat, fill = KG_class)) +
   geom_point(data = prec_mask_mean[dataset_agreement == 'high', .(lon, lat)],  aes(lon, lat)) +
   scale_fill_manual(values = colset_mid_qual[3:5]) +
-  labs(fill = 'Precipitation')  +
-  theme_light() +
   coord_sf(default_crs = sf::st_crs(3395)) + #Mercator
-  scale_x_continuous(labels = function(x) paste0(x, '\u00B0'), expand = c(0, 0)) +
-  scale_y_continuous(labels = function(x) paste0(x, '\u00B0'), expand = c(0, 0)) +
-  theme(plot.background = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.grid.major = element_blank())
+  labs(fill = 'Precipitation')  +
+  theme_light()
 
 ggplot(prec_mask_mean) +
   geom_bar(aes(x = KG_class)) + 
