c6c30df271ca1a759c1819de623d475b471e0972
imarkonis <you@example.comimarkonis@gmail.com>
2023-04-06 10:01:35 +0000

    examining dataset agreement

--- projects/partition_prec/04a_partition_prec_datasets.R
+++ projects/partition_prec/04a_partition_prec_datasets.R
@@ -27,21 +27,70 @@ land_cover_class <- merge(prec_mask[, .(lat, lon, land_cover_short_class)],
 land_cover_class_global <- land_cover_class[, .(prec_volume = sum(prec_volume)), 
                                      .(dataset, land_cover_short_class, year)]
 
-land_cover_class <- merge(prec_mask[, .(lat, lon, land_cover_short_class)], 
+biome_class <- merge(prec_mask[, .(lat, lon, biome_short_class)], 
                           prec_datasets_volume[, .(lon, lat, year, prec_volume, dataset)], 
                           by = c("lon", "lat"))
-land_cover_class_global <- land_cover_class[, .(prec_volume = sum(prec_volume)), 
-                                     .(dataset, land_cover_short_class, year)]
+biome_class_global <- biome_class[, .(prec_volume = sum(prec_volume)), 
+                                     .(dataset, biome_short_class, year)]
+
+elev_class <- merge(prec_mask[, .(lat, lon, elev_class)], 
+                          prec_datasets_volume[, .(lon, lat, year, prec_volume, dataset)], 
+                          by = c("lon", "lat"))
+elev_class_global <- elev_class[, .(prec_volume = sum(prec_volume)), 
+                                     .(dataset, elev_class, year)]
+
+prec_class <- merge(prec_mask[, .(lat, lon, prec_quant)], 
+                          prec_datasets_volume[, .(lon, lat, year, prec_volume, dataset)], 
+                          by = c("lon", "lat"))
+prec_class_global <- prec_class[, .(prec_volume = sum(prec_volume)), 
+                                     .(dataset, prec_quant, year)]
 
 ## Plots
 ### Land use
 ggplot(land_cover_class_global, aes(x = land_cover_short_class, y = prec_volume )) +
   geom_violin(fill = NA) +
-  geom_boxplot(width = .2, alpha = .6, fatten = NULL, show.legend = FALSE) +
-  geom_jitter(width = 0.1, alpha = .1) +
+  geom_boxplot(width = .2, alpha = .7, fatten = NULL, show.legend = FALSE) +
+  geom_jitter(width = 0.1, alpha = .05) +
   stat_summary(fun.data = "mean_se", geom = "pointrange", show.legend = F, 
                position = position_dodge(.175)) +
   scale_x_discrete(name = "Land cover class") +
-  scale_y_continuous(name = "Volume (km3)") +
+  scale_y_continuous(name = bquote('Precipitation ['~km^3~year^-1~']')) +
   facet_wrap(~land_cover_short_class, scales = 'free') +
-  theme_minimal()
+  theme_minimal() +
+  theme(axis.text.x = element_blank()) 
+
+ggplot(biome_class_global, aes(x = biome_short_class, y = prec_volume )) +
+  geom_violin(fill = NA) +
+  geom_boxplot(width = .2, alpha = .7, fatten = NULL, show.legend = FALSE) +
+  geom_jitter(width = 0.1, alpha = .05) +
+  stat_summary(fun.data = "mean_se", geom = "pointrange", show.legend = F, 
+               position = position_dodge(.175)) +
+  scale_x_discrete(name = "Biome type") +
+  scale_y_continuous(name = bquote('Precipitation ['~km^3~year^-1~']')) +
+  facet_wrap(~biome_short_class, scales = 'free') +
+  theme_minimal() +
+  theme(axis.text.x = element_blank()) 
+
+ggplot(elev_class_global, aes(x = elev_class, y = prec_volume )) +
+  geom_violin(fill = NA) +
+  geom_boxplot(width = .2, alpha = .7, fatten = NULL, show.legend = FALSE) +
+  geom_jitter(width = 0.1, alpha = .05) +
+  stat_summary(fun.data = "mean_se", geom = "pointrange", show.legend = F, 
+               position = position_dodge(.175)) +
+  scale_x_discrete(name = "Elevation zone") +
+  scale_y_continuous(name = bquote('Precipitation ['~km^3~year^-1~']')) +
+  facet_wrap(~elev_class, scales = 'free') +
+  theme_minimal() +
+  theme(axis.text.x = element_blank()) 
+
+ggplot(prec_class_global, aes(x = prec_quant, y = prec_volume )) +
+  geom_violin(fill = NA) +
+  geom_boxplot(width = .2, alpha = .7, fatten = NULL, show.legend = FALSE) +
+  geom_jitter(width = 0.1, alpha = .05) +
+  stat_summary(fun.data = "mean_se", geom = "pointrange", show.legend = F, 
+               position = position_dodge(.175)) +
+  scale_x_discrete(name = "Precipitaiton quantile") +
+  scale_y_continuous(name = bquote('Precipitation ['~km^3~year^-1~']')) +
+  facet_wrap(~prec_quant, scales = 'free') +
+  theme_minimal() +
+  theme(axis.text.x = element_blank()) 
---
+++ projects/partition_prec/05c_dataset_agreement_vol.R
@@ -0,0 +1,34 @@
+# Partition precipitation to different dataset agreement classes and quantify their uncertainty
+source('source/partition_prec.R')
+
+## Data 
+source('source/partition_prec.R')
+source('source/geo_functions.R')
+
+## Data 
+prec_mask <- readRDS(paste0(PATH_SAVE_PARTITION_PREC, "prec_masks.rds"))
+prec_datasets <- readRDS(paste0(PATH_SAVE_PARTITION_PREC, "prec_mean_datasets.rds"))
+prec_grid <- readRDS(paste0(PATH_SAVE_PARTITION_PREC, "prec_mean_volume_grid.rds"))
+
+## Variables
+dataset_agreement <- merge(prec_mask[, .(lon, lat, rel_dataset_agreement)], 
+                           prec_grid[, .(lon, lat, area)], 
+                           by = c("lon", "lat"))
+dataset_agreement <- merge(dataset_agreement, prec_datasets[dataset %in% PREC_GLOBAL_DATASETS], by = c("lon", "lat"))
+dataset_agreement[, prec_volume_year := area * M2_TO_KM2 * prec_mean * MM_TO_KM
+][, prec_mean := NULL][, prec_sd := NULL][, area := NULL] # km3
+
+
+## Analysis
+sd(dataset_agreement[rel_dataset_agreement == 'low' | rel_dataset_agreement == 'below average', 
+                     sum(prec_volume_year), .(dataset)]$V1)
+
+sd(dataset_agreement[rel_dataset_agreement != 'low' & rel_dataset_agreement != 'below average', 
+                     sum(prec_volume_year), .(dataset)]$V1)
+
+dataset_agreement_spread <- dataset_agreement[, .(prec_volume_year = sum(prec_volume_year)), .(rel_dataset_agreement, dataset)]
+dataset_agreement_spread <- dataset_agreement_spread[, .(prec_mean = round(mean(prec_volume_year), 0),
+                                                         prec_sd = round(sd(prec_volume_year), 0), 
+                                                         cv = round(sd(prec_volume_year) / mean(prec_volume_year), 2)), 
+                                                     rel_dataset_agreement]
+dataset_agreement_spread <- dataset_agreement_spread[c(3, 2, 1, 4, 5)]
