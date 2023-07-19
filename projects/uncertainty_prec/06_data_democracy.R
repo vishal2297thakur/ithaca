eba396f224f22d9c3f4481b2eef5d5d51b1a3f82
Mijael Vargas <mirovago@gmail.com>
2023-07-19 12:19:19 +0200

    data as ts

--- projects/uncertainty_prec/06_data_democracy.R
+++ projects/uncertainty_prec/06_data_democracy.R
@@ -16,22 +16,41 @@ masks_global <- readRDS(paste0(PATH_SAVE_UNCERTAINTY_PREC_SPATIAL,
                                "pRecipe_masks.rds"))
 
 ## Analysis
-### Normal masks
-prec_data <- prec_data[masks_global[!is.na(country) & country != "Antarctica",
+### Countries
+prec_country <- prec_data[masks_global[!is.na(country) & country != "Antarctica",
                                     .(lon, lat, country)], on = .(lon, lat)]
-prec_data <- prec_data[complete.cases(prec_data)]
 
-prec_data[, median_prec := median(prec, na.rm = TRUE), by = .(lon, lat, date)]
+prec_country <- prec_country[complete.cases(prec_country)]
 
-prec_data <- prec_data[, .(rmse_prec = rmse(median_prec, prec), country),
-                       .(lon, lat, dataset)] %>% unique()
+prec_country[, median_prec := median(prec, na.rm = TRUE),
+             by = .(lon, lat, date)]
 
-prec_data <- prec_data[ , .SD[which.min(rmse_prec)], by = .(lon, lat)]
-prec_data$dataset <- as.factor(prec_data$dataset)
-prec_data[, prec_sident := Mode(dataset)[[1]], by = "country"]
+prec_country <- prec_country[, .(rmse_prec = rmse(median_prec, prec), country),
+                             .(lon, lat, dataset)] %>% unique()
 
-democracy <- unique(prec_data[, .(country, prec_sident)])
+prec_country <- prec_country[ , .SD[which.min(rmse_prec)], by = .(lon, lat)]
+prec_country$dataset <- as.factor(prec_country$dataset)
+prec_country[, prec_sident := Mode(dataset)[[1]], by = "country"]
 
+prec_country <- unique(prec_country[, .(country, prec_sident)])
+
+### Basins
+prec_basins <- prec_data[masks_global[!is.na(basin_id), .(lon, lat, basin_id)],
+                         on = .(lon, lat)]
+
+prec_basins <- prec_basins[complete.cases(prec_basins)]
+
+prec_basins[, median_prec := median(prec, na.rm = TRUE), by = .(lon, lat, date)]
+
+prec_basins <- prec_basins[, .(rmse_prec = rmse(median_prec, prec), basin_id),
+                           .(lon, lat, dataset)] %>% unique()
+
+prec_basins <- prec_basins[ , .SD[which.min(rmse_prec)], by = .(lon, lat)]
+prec_basins$dataset <- as.factor(prec_basins$dataset)
+prec_basins[, prec_sident := Mode(dataset)[[1]], by = "basin_id"]
+
+prec_basins <- unique(prec_basins[, .(basin_id, prec_sident)])
 
 ## Save
-saveRDS(prec_data, paste0(PATH_SAVE_UNCERTAINTY_PREC, "democracy.rds"))
+save(prec_country, prec_basins,
+     file = paste0(PATH_SAVE_UNCERTAINTY_PREC, "democracy.rds"))
