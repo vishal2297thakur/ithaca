966eff1970afe3fd038e714acc1626109ed61348
imarkonis <you@example.comimarkonis@gmail.com>
2023-09-05 05:42:30 +0000

    path edits

--- projects/era5land_budget/01_dataset_paths.R
+++ projects/era5land_budget/01_dataset_paths.R
@@ -1,7 +1,4 @@
-# Reads and subsets data as netcdf files for the specified period.
-
 source('source/main.R')
-source('source/geo_functions.R')
 source('source/era5land_budget.R')
 
 library(pRecipe)
@@ -12,7 +9,7 @@ prec_datasets_fpaths <- unique(grep(paste(prec_dataset_names, collapse = "|"),
                              prec_all_fpaths, value = TRUE))
 prec_datasets_fpaths <- grep("land", prec_datasets_fpaths, value = TRUE)
 prec_datasets_fpaths <- grep("monthly", prec_datasets_fpaths, value = TRUE)
-prec_datasets_fpaths <- prec_datasets_fpaths[c(1, 3, 2, 6, 4, 5)]
+prec_datasets_fpaths <- prec_datasets_fpaths[c(1, 4, 3, 7, 5, 6)]
 
 evap_all_fpaths <- c(list.files(path = PATH_EVAP_SIM, full.names = TRUE),
               list.files(path = PATH_EVAP_OBS, full.names = TRUE))
@@ -20,7 +17,7 @@ evap_datasets_fpaths <- unique(grep(paste(evap_dataset_names, collapse = "|"),
                              evap_all_fpaths, value = TRUE))
 evap_datasets_fpaths <- grep("land", evap_datasets_fpaths, value = TRUE)
 evap_datasets_fpaths <- grep("monthly", evap_datasets_fpaths, value = TRUE)
-evap_datasets_fpaths <- evap_datasets_fpaths[c(1, 6, 2, 4)]
+evap_datasets_fpaths <- evap_datasets_fpaths[c(1, 7, 3, 5)]
 
 runoff_all_fpaths <- list.files(path = PATH_RUNOFF_SIM, full.names = TRUE)
 runoff_datasets_fpaths <- unique(grep(paste(runoff_dataset_names, collapse = "|"), 
