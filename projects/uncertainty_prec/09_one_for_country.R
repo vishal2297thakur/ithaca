50c8409e7c56245092b4550b04f9eae533c9b1e4
mirovago <mirovago@gmail.com>
2023-10-13 03:44:17 +0000

    country level

--- projects/uncertainty_prec/09_one_for_country.R
+++ projects/uncertainty_prec/09_one_for_country.R
@@ -1,4 +1,4 @@
-# Best data set per biome
+# Best data set per Country
 source("source/uncertainty_prec.R")
 
 ## Data
@@ -12,8 +12,7 @@ prec_masks <- readRDS(paste0(PATH_SAVE_UNCERTAINTY_PREC_SPATIAL,
 ## Analysis
 ### Prepare mask
 prec_masks <- prec_masks[unique(prec_years[, .(lon, lat)]),
-                         .(lon, lat, biome_class, biome_short_class),
-                         on = .(lon, lat)]
+                         .(lon, lat, country), on = .(lon, lat)]
 
 lonlat_area <- unique(prec_masks[, .(lon, lat)]) %>% .[, val := 1] %>%
   rasterFromXYZ(res = c(0.25, 0.25)) %>% area() %>% tabular() %>%
@@ -28,68 +27,66 @@ prec_years <- merge(prec_years, prec_masks, by = c("lon", "lat"))
 
 ###
 MC_month <- foreach (idx = 1:10000, .combine = rbind) %dopar% {
-  lonlat_sample <- split(prec_masks, by = "biome_short_class")
+  lonlat_sample <- split(prec_masks, by = "country")
   lonlat_sample <- lapply(lonlat_sample, function(x) {
     MIN_N <- nrow(x)
-    dummie <- x[, .SD[sample(.N, MIN_N%/%10)], by = biome_short_class]
+    dummie <- x[, .SD[sample(.N, ifelse(MIN_N%/%10 == 0, 1, MIN_N%/%10))],
+                by = country]
     dummie
   })
   lonlat_sample <- rbindlist(lonlat_sample)
   dummie <- prec_month[lonlat_sample[, .(lon, lat)], on = .(lon, lat)]
-  dummie[, area_biome := sum(area), .(dataset, biome_short_class)
-         ][, area_weights := area/area_biome
-           ][, weighted_t := t_prec*area_weights]
+  dummie[, area_class := sum(area), .(dataset, country)
+  ][, area_weights := area/area_class
+  ][, weighted_t := t_prec*area_weights]
   dummie <- dummie[, .(prec_t = sum(weighted_t, na.rm = TRUE)),
-                   .(dataset, biome_short_class)]
+                   .(dataset, country)]
   return(dummie)
 }
 
 
 MC_years <- foreach (idx = 1:10000, .combine = rbind) %dopar% {
-  lonlat_sample <- split(prec_masks, by = "biome_short_class")
+  lonlat_sample <- split(prec_masks, by = "country")
   lonlat_sample <- lapply(lonlat_sample, function(x) {
     MIN_N <- nrow(x)
-    dummie <- x[, .SD[sample(.N, MIN_N%/%10)], by = biome_short_class]
+    dummie <- x[, .SD[sample(.N, ifelse(MIN_N%/%10 == 0, 1, MIN_N%/%10))],
+                by = country]
     dummie
   })
   lonlat_sample <- rbindlist(lonlat_sample)
   dummie <- prec_years[lonlat_sample[, .(lon, lat)], on = .(lon, lat)]
-  dummie[, area_biome := sum(area), .(dataset, biome_short_class)
-         ][, area_weights := area/area_biome
-           ][, weighted_t := t_prec*area_weights]
+  dummie[, area_class := sum(area), .(dataset, country)
+  ][, area_weights := area/area_class
+  ][, weighted_t := t_prec*area_weights]
   dummie <- dummie[, .(prec_t = sum(weighted_t, na.rm = TRUE)),
-                   .(dataset, biome_short_class)]
+                   .(dataset, country)]
   return(dummie)
 }
 
 ##
-
-prec_month[, area_biome := sum(area), .(dataset, biome_short_class)
-           ][, area_weights := area/area_biome
-             ][, weighted_t := t_prec*area_weights]
+prec_month[, area_class := sum(area), .(dataset, country)
+][, area_weights := area/area_class
+][, weighted_t := t_prec*area_weights]
 
 prec_month <- prec_month[, .(prec_t = sum(weighted_t, na.rm = TRUE)),
-                         .(dataset, biome_short_class)]
+                         .(dataset, country)]
 
-prec_years[, area_biome := sum(area), .(dataset, biome_short_class)
-           ][, area_weights := area/area_biome
+prec_years[, area_class := sum(area), .(dataset, country)
+           ][, area_weights := area/area_class
              ][, weighted_t := t_prec*area_weights]
 
 prec_years <- prec_years[, .(prec_t = sum(weighted_t, na.rm = TRUE)),
-                         .(dataset, biome_short_class)]
+                         .(dataset, country)]
 
 ## Save
-
 fwrite(prec_month,
-       file = paste0(PATH_SAVE_UNCERTAINTY_PREC_TABLES,
-                     "biome_month.csv"))
+       file = paste0(PATH_SAVE_UNCERTAINTY_PREC_TABLES,"country_month.csv"))
 
 fwrite(prec_years,
-       file = paste0(PATH_SAVE_UNCERTAINTY_PREC_TABLES,
-                     "biome_years.csv"))
+       file = paste0(PATH_SAVE_UNCERTAINTY_PREC_TABLES,"country_years.csv"))
 
 saveRDS(MC_month, file = paste0(PATH_SAVE_UNCERTAINTY_PREC,
-                                "prec_biome_month.rds"))
+                                "prec_country_month.rds"))
 
 saveRDS(MC_years, file = paste0(PATH_SAVE_UNCERTAINTY_PREC,
-                                "prec_biome_years.rds"))
+                                "prec_country_years.rds"))
