48f1190869531fc26ae6bf798a2688175fd9beb4
imarkonis <you@example.comimarkonis@gmail.com>
2023-10-24 14:11:14 +0000

    lon lat bug fixed

--- projects/med_arid/prec_som.R
+++ projects/med_arid/prec_som.R
@@ -5,13 +5,13 @@ library(ggplot2)
 prec <- readRDS('../../shared/data_projects/ithaca/med_prec/pr_year_1980_2019.rds')
 
 #Data pre-processing
-names(prec)[c(1, 2, 6)] <- c('lat', 'lon', 'dataset') #all variables must be lowercase & lon/lat are reversed
+names(prec)[6] <- c('dataset') #all variables must be lowercase 
 prec[, period := NULL]
 prec <- prec[!duplicated(prec)] 
 
 dataset_names_40yr <- prec[year < 1990, unique(dataset)] 
 prec_40yr <- prec[dataset %in% dataset_names_40yr]
-prec_40yr_mean <- prec_40yr[, .(value = mean(pr_year)), .(year, lon, lat)]
+prec_40yr_mean <- prec_40yr[, .(value = mean(pr_year)), .(year, lat, lon)]
 
 #Self-Organizing Map (SOM)
 dim_1 <- 3
@@ -21,19 +21,21 @@ n_iterations <- 10^4
 
 ##Dataset mean
 inp_som <- sominp(prec_40yr_mean)
-prec_40yr_som <- somspa(inp_som, rlen = node, grid = somgrid(dim_1, dim_2, "hexagonal"))
+prec_40yr_som <- somspa(inp_som, rlen = n_iterations, grid = somgrid(dim_1, dim_2, "hexagonal"))
 plot(prec_40yr_som)
+my_regions <- somregs(prec_40yr_som, nregions = 9) 
+plot(my_regions, 2:9, 3, 3)
 
 ##ERA5
-prec_era5 <- prec_40yr[dataset == dataset_names_40yr[1], .(year, lon, lat, value = pr_year)]
+prec_era5 <- prec_40yr[dataset == dataset_names_40yr[1], .(year, lat, lon, value = pr_year)]
 inp_som <- sominp(prec_era5)
-era5_som <- somspa(inp_som, rlen = node, grid = somgrid(dim_1, dim_2, "hexagonal"))
+era5_som <- somspa(inp_som, rlen = n_iterations, grid = somgrid(dim_1, dim_2, "hexagonal"))
 plot(era5_som)
 
 ##MERRA2
-prec_merra2 <- prec_40yr[dataset == dataset_names_40yr[2], .(year, lon, lat, value = pr_year)]
+prec_merra2 <- prec_40yr[dataset == dataset_names_40yr[2], .(year, lat, lon,value = pr_year)]
 inp_som <- sominp(prec_merra2)
-merra2_som <- somspa(inp_som, rlen = node, grid = somgrid(dim_1, dim_2, "hexagonal"))
+merra2_som <- somspa(inp_som, rlen = n_iterations, grid = somgrid(dim_1, dim_2, "hexagonal"))
 plot(merra2_som)
 
 #Slopes
