b39e917914acd436006e8f38904215171e0cdc98
jorub <reintheoretisch@gmail.com>
2023-10-25 09:13:07 +0000

    Softcoded thresholds and removed unused chunk

--- projects/partition_evap/04_add_classes.R
+++ projects/partition_evap/04_add_classes.R
@@ -23,20 +23,18 @@ evap_stats[, evap_quant := ordered(quantcut(ens_mean_mean, 10),
 evap_stats[, quant_ens_cv := ordered(quantcut(ens_mean_cv, 5), 
                                          labels = c('0-0.2', '0.2-0.4', '0.4-0.6', '0.6-0.8', '0.8-1.00')), evap_quant]
 
-evap_stats[std_quant_range <= 0.11, rel_dataset_agreement := ordered(1, labels = "high")] 
-evap_stats[std_quant_range > 0.11 & std_quant_range <= 0.18, rel_dataset_agreement := ordered(3, labels = "above average")]
-evap_stats[std_quant_range > 0.18 & std_quant_range <= 0.38, rel_dataset_agreement := ordered(4, labels = "average")]
-evap_stats[std_quant_range > 0.38 & std_quant_range <= 0.61, rel_dataset_agreement := ordered(5, labels = "below average")]
-evap_stats[std_quant_range > 0.61, rel_dataset_agreement := ordered(7, labels = "low")]
 
-evap_stats[std_quant_range <= 0.1, abs_dataset_agreement := ordered(1, labels = "high")] 
-evap_stats[std_quant_range > 0.1 & std_quant_range <= 0.25, abs_dataset_agreement := ordered(3, labels = "above average")]
-evap_stats[std_quant_range > 0.25 & std_quant_range <= 0.5, abs_dataset_agreement := ordered(4, labels = "average")]
-evap_stats[std_quant_range > 0.5 & std_quant_range <= 1, abs_dataset_agreement := ordered(5, labels = "below average")]
-evap_stats[std_quant_range > 1, abs_dataset_agreement := ordered(7, labels = "low")]
+## relative dataset agreement at quantile 0.1, 0.3, 0.7. 0.9
+quant_thr_0_1 <- quantile(evap_stats$std_quant_range, c(0.1))
+quant_thr_0_3 <- quantile(evap_stats$std_quant_range, c(0.3))
+quant_thr_0_7 <- quantile(evap_stats$std_quant_range, c(0.7))
+quant_thr_0_9 <- quantile(evap_stats$std_quant_range, c(0.9))
 
-#evap_stats[, evap_quant_dataset_agreement := ordered(quantcut(std_quant_range, c(0, 0.1, 0.3, 0.7, 0.9, 1)),
-#                                                          labels = c('high', 'above average', 'average', 'below average', 'low')), evap_quant]
+evap_stats[std_quant_range <= quant_thr_0_1, rel_dataset_agreement := ordered(1, labels = "high")] 
+evap_stats[std_quant_range > quant_thr_0_1 & std_quant_range <= quant_thr_0_3, rel_dataset_agreement := ordered(3, labels = "above average")]
+evap_stats[std_quant_range > quant_thr_0_3 & std_quant_range <= quant_thr_0_7, rel_dataset_agreement := ordered(4, labels = "average")]
+evap_stats[std_quant_range > quant_thr_0_7 & std_quant_range <= quant_thr_0_9, rel_dataset_agreement := ordered(5, labels = "below average")]
+evap_stats[std_quant_range > quant_thr_0_9, rel_dataset_agreement := ordered(7, labels = "low")]
 
 evap_stats[std_quant_range <= 0.1, evap_quant_dataset_agreement := ordered(1, labels = "high")] 
 evap_stats[std_quant_range > 0.1 & std_quant_range <= 0.3, evap_quant_dataset_agreement := ordered(3, labels = "above average")]
@@ -45,8 +43,19 @@ evap_stats[std_quant_range > 0.7 & std_quant_range <= 0.9, evap_quant_dataset_ag
 evap_stats[std_quant_range > 0.9, evap_quant_dataset_agreement := ordered(7, labels = "low")]
 
 evap_masks <- merge(masks_global, evap_stats[, .(lon, lat, evap_quant, rel_dataset_agreement, #Merges only complete cases
-                               abs_dataset_agreement, 
                                evap_quant_dataset_agreement)], by = c("lon", "lat"))
 
 ## Save data
 saveRDS(evap_masks, paste0(PATH_SAVE_PARTITION_EVAP, "evap_masks.rds"))
+
+
+## Code review
+evap_masks <- readRDS(paste0(PATH_SAVE_PARTITION_EVAP, "evap_masks.rds"))
+
+ggplot(evap_masks)+
+  geom_tile(aes(x = lon, y = lat, fill = rel_dataset_agreement))+
+  theme_bw()
+
+ggplot(evap_masks)+
+  geom_tile(aes(x = lon, y = lat, fill = evap_quant_dataset_agreement))+
+  theme_bw()
