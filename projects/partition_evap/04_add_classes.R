c136f88b3b6b7ce0624c9c7e2da290a8148ad4a4
jorub <reintheoretisch@gmail.com>
2023-11-22 06:34:23 +0000

    Filtering IPCC reference region with less than 10 grids to resolve agreement index

--- projects/partition_evap/04_add_classes.R
+++ projects/partition_evap/04_add_classes.R
@@ -24,7 +24,7 @@ evap_stats[, quant_ens_cv := ordered(quantcut(ens_mean_cv, 5),
                                          labels = c('0-0.2', '0.2-0.4', '0.4-0.6', '0.6-0.8', '0.8-1.00')), evap_quant]
 
 
-## relative dataset agreement at quantile 0.1, 0.3, 0.7. 0.9
+### Relative dataset agreement at quantile 0.1, 0.3, 0.7. 0.9
 quant_thr_0_1 <- quantile(evap_stats$std_quant_range, c(0.1))
 quant_thr_0_3 <- quantile(evap_stats$std_quant_range, c(0.3))
 quant_thr_0_7 <- quantile(evap_stats$std_quant_range, c(0.7))
@@ -36,18 +36,10 @@ evap_stats[std_quant_range > quant_thr_0_3 & std_quant_range <= quant_thr_0_7, r
 evap_stats[std_quant_range > quant_thr_0_7 & std_quant_range <= quant_thr_0_9, rel_dataset_agreement := ordered(5, labels = "below average")]
 evap_stats[std_quant_range > quant_thr_0_9, rel_dataset_agreement := ordered(7, labels = "low")]
 
-#evap_stats[, evap_quant_dataset_agreement := ordered(quantcut(std_quant_range, c(0, quant_thr_0_1, quant_thr_0_3, quant_thr_0_7, quant_thr_0_9, 1)),
-#                                                     labels = c('high', 'above average', 'average', 'below average', 'low')), evap_quant]
 
 evap_stats[, evap_quant_dataset_agreement := ordered(quantcut(std_quant_range, c(0, 0.1, 0.3, 0.7, 0.9, 1)),
                                                      labels = c('high', 'above average', 'average', 'below average', 'low')), evap_quant]
 
-
-#evap_stats[std_quant_range <= 0.1, evap_quant_dataset_agreement := ordered(1, labels = "high")] 
-#evap_stats[std_quant_range > 0.1 & std_quant_range <= 0.3, evap_quant_dataset_agreement := ordered(3, labels = "above average")]
-#evap_stats[std_quant_range > 0.3 & std_quant_range <= 0.7, evap_quant_dataset_agreement := ordered(4, labels = "average")]
-#evap_stats[std_quant_range > 0.7 & std_quant_range <= 0.9, evap_quant_dataset_agreement := ordered(5, labels = "below average")]
-#evap_stats[std_quant_range > 0.9, evap_quant_dataset_agreement := ordered(7, labels = "low")]
 
 evap_masks <- merge(masks_global, evap_stats[, .(lon, lat, evap_quant, rel_dataset_agreement, #Merges only complete cases
                                std_quant_range, evap_quant_dataset_agreement)], by = c("lon", "lat"))
@@ -61,9 +53,11 @@ evap_masks[, biome_dataset_agreement := ordered(quantcut(std_quant_range, c(0, 0
 evap_masks[, landcover_dataset_agreement := ordered(quantcut(std_quant_range, c(0, 0.1, 0.3, 0.7, 0.9, 1)),
                                                     labels = c('high', 'above average', 'average', 'below average', 'low')), land_cover_short_class]
 
+evap_masks[, grid_count_ipcc := .N, IPCC_ref_region]
+evap_masks[grid_count_ipcc < 10, IPCC_ref_region := NA]
+evap_masks[, ipcc_dataset_agreement := ordered(quantcut(std_quant_range, c(0, 0.1, 0.3, 0.7, 0.9, 1)),
+                                                    labels = c('high', 'above average', 'average', 'below average', 'low')), IPCC_ref_region]
 
-#evap_masks[, ipcc_dataset_agreement := ordered(quantcut(std_quant_range, c(0, 0.1, 0.3, 0.7, 0.9, 1)),
-#                                                    labels = c('high', 'above average', 'average', 'below average', 'low')), IPCC_ref_region]
 
 ## Save data
 saveRDS(evap_masks, paste0(PATH_SAVE_PARTITION_EVAP, "evap_masks.rds"))
@@ -80,3 +74,8 @@ ggplot(evap_masks)+
   geom_tile(aes(x = lon, y = lat, fill = evap_quant_dataset_agreement))+
   theme_bw()
 
+IPCC_test <- evap_masks[, .N, IPCC_ref_region]
+
+ggplot(IPCC_test)+
+  geom_bar(aes(y = N, x = IPCC_ref_region), stat = "identity")+
+  theme_bw()
