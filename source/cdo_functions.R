dbbdf0310b4d8fd3746c1b76c98b1c0df43ee792
jorub <reintheoretisch@gmail.com>
2024-01-16 07:24:36 +0000

    added more cdo functions

--- source/cdo_functions.R
+++ source/cdo_functions.R
@@ -178,6 +178,29 @@ cdo_timmean_fnc <- function(inputfile_name, outputfile_name){
   system(cmd_cdo)
 }
 
+## cdo standard deviation
+cdo_timstd_fnc <- function(inputfile_name, outputfile_name){
+  if(!is.character(inputfile_name)){
+    stop(paste(inputfile_name, "is not a valid filename"))
+  }
+  
+  if(!is.character(outputfile_name)){
+    stop(paste(outputfile_name, "is not a valid filename"))
+  }
+  
+  file_extension <- strsplit(basename(inputfile_name), split="\\.")[[1]]
+  if(file_extension[-1] != "nc"){
+    stop(paste(basename(inputfile_name), "not a nc file"))
+  }
+  
+  file_extension <- strsplit(basename(outputfile_name), split="\\.")[[1]]
+  if(file_extension[-1] != "nc"){
+    stop(paste(basename(outputfile_name), "not a nc file"))
+  }
+  cmd_cdo <- paste("cdo timstd", inputfile_name, outputfile_name)
+  system(cmd_cdo)
+}
+
 ## cdo info to text
 cdo_info_to_text_fnc <- function(inputfile_name, outputfile_name){
   if(!is.character(inputfile_name)){
@@ -217,4 +240,35 @@ cdo_info_to_text_fnc <- function(inputfile_name, outputfile_name){
   data <- data.table(date = as.Date(date), value = value)
   data <- data[complete.cases(data)]
   return(data)
+}
+
+cdo_change_field_fnc <- function(inputfile_name, outputfile_name, old_var_name, new_var_name, new_long_name, old_unit, new_unit){
+  if(!is.character(inputfile_name)){
+    stop(paste(inputfile_name, "is not a valid filename"))
+  }
+  
+  if(!is.character(outputfile_name)){
+    stop(paste(outputfile_name, "is not a valid filename"))
+  }
+  
+  file_extension <- strsplit(basename(inputfile_name), split="\\.")[[1]]
+  if(file_extension[-1] != "nc"){
+    stop(paste(basename(inputfile_name), "not a nc file"))
+  }
+  
+  file_extension <- strsplit(basename(outputfile_name), split="\\.")[[1]]
+  if(file_extension[-1] != "nc"){
+    stop(paste(basename(outputfile_name), "not a nc file"))
+  }
+  
+  cdo_cmd <- paste0("cdo -chname,",old_var_name,",",new_var_name, " -chunit,",old_unit,",",new_unit," ", inputfile_name," ", outputfile_name)
+  print(cdo_cmd)
+  system(cdo_cmd)
+  
+  nc_cmd <- paste0("ncatted -a long_name,",new_var_name,",o,c,",new_long_name, " " , outputfile_name)
+  system(nc_cmd)
+  print(nc_cmd)
+  print("Your new file attributes are")
+  cmd_cdo <- paste0("cdo vardes ",outputfile_name)
+  system(cmd_cdo)
 }
