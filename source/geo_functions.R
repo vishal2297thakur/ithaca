cdc8dd6f6a43de571e04c161984f0b0d1a631f89
jorub <reintheoretisch@gmail.com>
2024-06-10 14:54:08 +0000

    adding robust function to convert raster to table

--- source/geo_functions.R
+++ source/geo_functions.R
@@ -97,6 +97,21 @@ brick_to_dt <- function(x){
   return(x_dt)
 }
 
+# Transform a raster object to data.table format
+
+raster_to_dt <- function(x) {
+  no_cores <- detectCores() - 1
+  if (no_cores < 1 | is.na(no_cores))(no_cores <- 1)
+  registerDoParallel(cores = no_cores)
+  dummie <- foreach (idx = 1:nlayers(x), .combine = rbind) %dopar% {
+    dummie_layer <- x[[idx]]
+    dummie_layer <- as.data.frame(dummie_layer, xy = TRUE,long = TRUE, na.rm = TRUE) %>% as.data.table()
+    setnames(dummie_layer, c("lon", "lat", "date", "value"))
+    dummie_layer
+  }
+  return(dummie)
+}
+
 #' Grid area of each cell
 #'
 #' Function to compute area of each cell in m2
