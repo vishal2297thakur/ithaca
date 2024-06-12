9aff3f166df6bc5b4057baed11f4d62e9461ef8a
imarkonis <you@example.comimarkonis@gmail.com>
2024-06-12 09:39:14 +0000

    path changes

--- projects/paleo/01_import_data.R
+++ projects/paleo/01_import_data.R
@@ -2,4 +2,27 @@
 
 source('source/paleo.R')
 
-recon_summary <- read.csv()
+recon_summary <- read.csv(paste0(PATH_SAVE_PALEO, "/mnt/shared/data/paleo/processed/pratap_thesis/recon_meta.csv"))
+recon_data_xls <- readxl::read_xls(paste0(PATH_SAVE_PALEO, "/mnt/shared/data/paleo/processed/pratap_thesis/recon_data.xls"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+saveRDS(recon_data, paste0(PATH_SAVE_PALEO, "recon_data.rds"))
---
+++ projects/paleo/02_summary_stats.R
@@ -0,0 +1,5 @@
+## Estimates the reconstruction statistics that will be used for comparison with observations
+
+source('source/paleo.R')
+
+recon_data <- readRDS( paste0(PATH_SAVE_PALEO, "recon_data.rds"))
