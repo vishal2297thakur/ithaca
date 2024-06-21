de00c37afa1aa9767c74744e4d86424e65e34af4
imarkonis <you@example.comimarkonis@gmail.com>
2024-06-21 15:20:08 +0000

    ExEvEs implications / wet days

--- projects/exeves/dev/czechia_revision/06_drivers.R
+++ projects/exeves/dev/czechia_revision/06_drivers.R
@@ -62,6 +62,127 @@ ggarrange(gg_rad, NULL, gg_prec,
                      legend = 'bottom', common.legend = TRUE) + bgcolor("white")  
 ggsave(paste0(PATH_OUTPUT_FIGURES, "drivers.png"), width = 7, height = 11)
 
+
+# Wet days / Dry days
+
+all_prec_days <- exeves_drivers[, .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day, month(date))]
+colnames(all_prec_days)[4] <- "value"
+all_prec_days$conditions <- "All days"
+
+exeves_prec_days <- exeves_drivers[!is.na(event_80_95_id), .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day, month(date))]
+colnames(exeves_prec_days)[4] <- "value"
+exeves_prec_days$conditions <- "ExEvEs"
+
+non_exeves_prec_days <- exeves_drivers[is.na(event_80_95_id), .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day, month(date))]
+colnames(non_exeves_prec_days)[4] <- "value"
+non_exeves_prec_days$conditions <- "Non-ExEvEs"
+
+prec_days <- rbind(all_prec_days, exeves_prec_days, non_exeves_prec_days)
+prec_days <- prec_days[, c(1, 3:2, 5:4)]
+
+ggplot(prec_days[conditions == 'ExEvEs']) +
+  geom_col(aes(x = period, y = value, fill = prec_day),
+           position = position_fill()) +
+  facet_wrap(~month)+
+  theme_linedraw() 
+
+
+exeves_drivers[, prec_day := factor("wet")]
+exeves_drivers[prec < 1, prec_day := factor("dry")]
+
+
+exeves_drivers[prec_day == 'dry', .N/(161 * 0.5 * PERIOD_LENGTH), .(period)]
+exeves_drivers[!is.na(event_80_95_id), .N/(161 * 0.5 * PERIOD_LENGTH), .(period)]
+
+to_plot <- exeves_drivers[prec_day == 'dry', .N/(SUB_PERIOD_YEARS), .(period, grid_id)]
+colnames(to_plot)[c(1, 3)] <- c("Period", "Dry Days")
+ggplot(to_plot) +
+  geom_boxplot(aes(x = Period, y = `Dry Days`)) +
+  theme_linedraw() 
+
+to_plot <- exeves_drivers[prec_day == 'dry', .N/(SUB_PERIOD_YEARS), .(period, grid_id,  factor(month(date)))]
+colnames(to_plot)[c(1, 3:4)] <- c("Period", "Month", "Dry Days")
+ggplot(to_plot) +
+  geom_boxplot(aes(x = Month, y = `Dry Days`, fill = Period)) +
+  theme_linedraw() 
+
+
+all_prec_days <- exeves_drivers[, .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day)]
+colnames(all_prec_days)[3] <- "value"
+all_prec_days$conditions <- "All days"
+
+exeves_prec_days <- exeves_drivers[!is.na(event_80_95_id), .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day)]
+colnames(exeves_prec_days)[3] <- "value"
+exeves_prec_days$conditions <- "ExEvEs"
+
+non_exeves_prec_days <- exeves_drivers[is.na(event_80_95_id), .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day)]
+colnames(non_exeves_prec_days)[3] <- "value"
+non_exeves_prec_days$conditions <- "Non-ExEvEs"
+
+exeves_prec_days <- rbind(all_prec_days, exeves_prec_days, non_exeves_prec_days)
+exeves_prec_days <- exeves_prec_days[, c(4, 1:3)]
+
+ggplot(exeves_prec_days) +
+  geom_col(aes(x = period, y = value, fill = prec_day, group = prec_day),
+           position = position_dodge()) +
+  facet_wrap(~conditions, scales = 'free')+
+  theme_linedraw() 
+
+all_prec_days <- exeves_drivers[month(date) == 6, .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day)]
+colnames(all_prec_days)[3] <- "value"
+all_prec_days$conditions <- "All days"
+
+exeves_prec_days <- exeves_drivers[!is.na(event_80_95_id) & month(date) == 6, .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day)]
+colnames(exeves_prec_days)[3] <- "value"
+exeves_prec_days$conditions <- "ExEvEs"
+
+non_exeves_prec_days <- exeves_drivers[is.na(event_80_95_id) & month(date) == 6, .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day)]
+colnames(non_exeves_prec_days)[3] <- "value"
+non_exeves_prec_days$conditions <- "Non-ExEvEs"
+
+exeves_prec_days <- rbind(all_prec_days, exeves_prec_days, non_exeves_prec_days)
+exeves_prec_days <- exeves_prec_days[, c(4, 1:3)]
+
+ggplot(exeves_prec_days) +
+  geom_col(aes(x = period, y = value, fill = prec_day, group = prec_day),
+           position = position_dodge()) +
+  facet_wrap(~conditions, scales = 'free')+
+  theme_linedraw() 
+
+all_prec_days <- exeves_drivers[month(date) == 1, .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day)]
+colnames(all_prec_days)[3] <- "value"
+all_prec_days$conditions <- "All days"
+
+exeves_prec_days <- exeves_drivers[!is.na(event_80_95_id) & month(date) == 1, .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day)]
+colnames(exeves_prec_days)[3] <- "value"
+exeves_prec_days$conditions <- "ExEvEs"
+
+non_exeves_prec_days <- exeves_drivers[is.na(event_80_95_id) & month(date) == 1, .N/(161 * 0.5 * PERIOD_LENGTH), .(period, prec_day)]
+colnames(non_exeves_prec_days)[3] <- "value"
+non_exeves_prec_days$conditions <- "Non-ExEvEs"
+
+exeves_prec_days <- rbind(all_prec_days, exeves_prec_days, non_exeves_prec_days)
+exeves_prec_days <- exeves_prec_days[, c(4, 1:3)]
+
+ggplot(exeves_prec_days) +
+  geom_col(aes(x = period, y = value, fill = prec_day, group = prec_day),
+           position = position_dodge()) +
+  facet_wrap(~conditions, scales = 'free')+
+  theme_linedraw() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 exeves_drivers[Conditions == 'ExEvE', mean(total_rad)] /
   exeves_drivers[, mean(total_rad)]
 
